38c7024ad88cda1e7546737ef6d30384d5bcaba9
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2022-11-30 23:25:10 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    makeTask(e.target.new_task.value)
+    form.reset()
+})
+  
+})
+
+function makeTask(task){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "del(X)"
+  li.textContent = task
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
